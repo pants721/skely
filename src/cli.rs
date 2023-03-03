1f4c74d6efc043d89e53dd1c69490fd896078b59
Lucas Newcomb <pantsbabyy@gmail.com>
2023-03-02 20:58:59 -0500

    Added more cli documentation

--- src/cli.rs
+++ src/cli.rs
@@ -23,7 +23,8 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(name = "sk")]
 #[command(bin_name = "sk")]
-#[command(about = "A command line tool for creating skeleton projects", long_about = None)]
+#[command(about = "A command line tool for using and managing skeleton projects", long_about = None)]
+#[command(author = "Lucas Newcomb (pants721)")]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -31,23 +32,38 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    List,
+    /// Lists all configured skeletons
+    List {
+        #[arg(short, long)]
+        verbose: bool,
+    },
+    /// Opens skeleton to edit
     #[command(arg_required_else_help = true)]
     Edit {
+        /// Id of skeleton to edit
         id: String,
     },
+    /// Adds skeleton to configured skeletons
     Add {
+        /// Name of skeleton
         #[arg(required = true)]
         name: String,
+        /// Optional source to create skeleton from
+        #[arg(short, long)]
         source: Option<PathBuf>,
     },
+    /// Creates a new project from specified skeleton
     New {
+        /// Id of skeleton to copy
         #[arg(required = true)]
         id: String,
+        /// Desired project path
         #[arg(required = true)]
         path: PathBuf,
     },
+    /// Removes skeleton
     Remove {
+        /// Id of skeleton to remove
         id: String,
     },
 }
